9d15711ced57d2ac47d95117ed76bf8492312adf
ericpkatz <ericpkatz@gmail.com>
2023-02-13 10:54:46 -0500

    fetch and display all routines

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 
 const App = ()=> {
+  const [ routines, setRoutines ] = useState([]);
+  useEffect(()=> {
+    fetch('https://fitnesstrac-kr.herokuapp.com/api/routines')
+      .then( response => response.json())
+      .then( routines => setRoutines(routines));
+  }, []);
   return (
     <div>
-      <h1>Front End</h1>
+      <h1>Fitness Tracker</h1>
+      <h2>Routines ({ routines.length })</h2>
+      <ul>
+        {
+          routines.map( routine => {
+            return (
+              <li key={ routine.id }>
+                { routine.name } ({ routine.activities.length})
+              </li>
+            );
+          })
+        }
+      </ul>
     </div>
   );
 };
 
-const root = createRoot(document.querySelector('#rot'));
+const root = createRoot(document.querySelector('#root'));
 
 root.render(<App />);
